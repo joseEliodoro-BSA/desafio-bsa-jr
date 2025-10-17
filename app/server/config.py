47622889a13725fbe8942ab4d1595c6c8a3b566c
jose eliodoro <jose.santana@bsatech.io>
2025-10-17 20:35:16 -0300

    fix: corrigindo nome de constante

--- app/server/config.py
+++ app/server/config.py
@@ -10,6 +10,6 @@ DATABASE_PORT = int(os.getenv("DATABASE_PORT", 5432))
 DATABASE_DB = str(os.getenv("DATABASE_DB", "desafio"))
 
 # Conexão ao banco como administrador (root)
-DATABASE_URI = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}"
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_DB}"
 
 # "postgresql://root:root@localhost:5432/desafio"
--- app/server/db.py
+++ app/server/db.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
-from app.server.config import DATABASE_URI
+from app.server.config import DATABASE_URL
 from contextlib import contextmanager
 from typing import Iterator
 # DATABASE_URI = "postgresql://root:root@localhost:5432/desafio"
 
-engine = create_engine(DATABASE_URI)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine) # conexão entre o banco é o código
 
 Base = declarative_base()
