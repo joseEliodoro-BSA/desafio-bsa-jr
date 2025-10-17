7c1cef90cf862bcbf708679793da2b5ab939d983
jose eliodoro <jose.santana@bsatech.io>
2025-10-17 09:52:03 -0300

    feat: adiciona valor unico para socket_id

--- app/server/models.py
+++ app/server/models.py
@@ -9,8 +9,10 @@ class User(Base):
   email = Column(String(255))
   username = Column(String(255))
   
-  socket_id = Column(String(100))
+  socket_id = Column(String(100), unique=True)
   connected= Column(Boolean, default=True)
   connected_at = Column(DateTime, default=datetime.now())
   last_seen = Column(DateTime, default=datetime.now())
   
+  def __str__(self):
+    return "User[username={}, socket_id={}]".format(self.username, self.socket_id)
