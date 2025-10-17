aa69853a34fb851dd867f87ec4a4f15b7f7f1450
jose eliodoro <jose85751696@gmail.com>
2025-10-16 21:25:55 -0300

    feat: adiciona função de fibonacci ao escopo do projeto

---
+++ app/server/utils.py
@@ -0,0 +1,28 @@
+def fibonacci(n: int) -> int:
+  """
+  Calcula o n-ésimo número da sequência de Fibonacci.
+
+  A sequência de Fibonacci é definida como:
+  F(0) = 0
+  F(1) = 1
+  F(n) = F(n-1) + F(n-2), para n > 1
+
+  Parameters:
+    n (int): A posição desejada na sequência de Fibonacci (n >= 0)
+
+  Returns:
+    int: O valor do n-ésimo número de Fibonacci
+  """
+  if(not(isinstance(n, int))):
+    raise TypeError("n só aceita valores inteiros positivos")
+  
+  if(n < 0):
+    raise ValueError("n não aceita valor negativo")
+
+  if n == 0: return 0
+
+  a, b = 0, 1
+  for _ in range(1, n):
+    a, b = b, a+b
+    
+  return b
