15bdcb20f3107bc7e035b5f8526686972bfcfd0d
jose eliodoro <jose.santana@bsatech.io>
2025-10-16 22:49:03 -0300

    feat: adiciona entidade de usuário

---
+++ app/server/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, String, Boolean, DateTime
+from app.server.db import Base
+from datetime import datetime
+
+class User(Base):
+  __tablename__ = "users"
+  id = Column(Integer, primary_key=True, index=True)
+  
+  email = Column(String(255))
+  username = Column(String(255))
+  
+  socket_id = Column(String(100))
+  connected= Column(Boolean, default=True)
+  connected_at = Column(DateTime, default=datetime.now())
+  last_seen = Column(DateTime, default=datetime.now())
+  
